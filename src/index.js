5465943add8fb68d2893842b43e2e357104146f5
luxi <lixu199603175795@gmail.com>
2017-02-08 15:33:45 +0800

    1

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+// // import { element } from './jsx/jsx.component';
+
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root')
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
+
+// const element = (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>
+// );
+
+// 数字 
+// import Game from './component/Game/Game'
+// ReactDOM.render(
+//   <Game/>,
+//   document.getElementById('root')
+// );
+
+
+// todolist 
+import App from './react-todo/app.js';
+ReactDOM.render(<App/>, document.getElementById("root"));
