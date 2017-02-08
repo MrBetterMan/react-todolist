ec5c44b0f56372b4a752746d83147eedd3e86a1d
luxi <lixu199603175795@gmail.com>
2017-02-09 01:37:02 +0800

    1111

--- src/index.js
+++ src/index.js
@@ -38,3 +38,38 @@ import ReactDOM from 'react-dom';
 // todolist 
 import App from './react-todo/app.js';
 ReactDOM.render(<App/>, document.getElementById("root"));
+
+
+
+
+// redux
+import {combineReducers, createStore } from 'redux';
+
+const userReducer = (state={}, action) => {
+    switch(action.type) {
+        case "CHANGE_NAME": 
+            state.name = action.payload;
+            break;
+        case "CHANGE_AGE": 
+            state.age = action.payload
+            break;    
+    }
+    return state;
+};
+const tweetsReducer = (state=[], action) => state;
+// const reducer = function(state, action) {
+//     if(action.type === "INC") {
+//         return state+1;
+//     }
+//     return state;
+// };
+const reducer = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer
+})
+const store = createStore(reducer);
+store.subscribe(() => {
+    console.log("store changed", store.getState())
+});
+store.dispatch({type: "CHANGE_NAME", payload: "will"});
+store.dispatch({type: "CHANGE_AGE", payload: 20});
